f88a835cb378ebbafc0accc93a47645ff740dff2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-06-16 21:37:00 -0500

    Initial commit

---
+++ src/Interface/ExceptionInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Wip\Interface;
+
+use Throwable;
+
+interface ExceptionInterface extends Throwable {}
---
+++ src/Interface/FooInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Wip\Interface;
+
+interface FooInterface {}
